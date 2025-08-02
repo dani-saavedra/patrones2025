cd350853b45c077d176ebde9061748f1c20a5157
Daniel Saavedra <daniel.saavedra.fon@gmail.com>
2025-08-02 08:41:37 -0500

    Initial commit

---
+++ src/main/java/co/edu/unisabana/patrones20252/Patrones20252Application.java
@@ -0,0 +1,13 @@
+package co.edu.unisabana.patrones20252;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Patrones20252Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Patrones20252Application.class, args);
+    }
+
+}
---
+++ src/test/java/co/edu/unisabana/patrones20252/Patrones20252ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unisabana.patrones20252;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Patrones20252ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
